3c6fe657ee46c602963445e2cab7c1ff6899ae5f
Marco Pessotto <melmothx@gmail.com>
2015-05-04 12:32:04 +0200

    Bumped up version for new option wild_matching

--- lib/Interchange/Search/Solr.pm
+++ lib/Interchange/Search/Solr.pm
@@ -17,11 +17,11 @@ Interchange::Search::Solr -- Solr query encapsulation
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 
 =head1 SYNOPSIS
