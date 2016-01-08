a28b798bf1e17bf5c8a90da42e7d9869027fb341
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-01-08 08:29:54 +0100

    Bump up version number.

--- lib/Interchange/Search/Solr.pm
+++ lib/Interchange/Search/Solr.pm
@@ -22,11 +22,11 @@ Interchange::Search::Solr -- Solr query encapsulation
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 DESCRIPTION
 
